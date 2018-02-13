3bf59d94c4203a6d680011d58c6ced06ec811947
gserrano <gserrano@nearsoft.com>
2018-02-13 14:06:05 -0600

    reorganized tests

--- test/XmlGenericConverterTest.elm
+++ test/XmlGenericConverterTest.elm
@@ -6,75 +6,95 @@ import XmlGenericConverter as C
 import XmlNode as X
 
 
-testPreparation : Test
-testPreparation =
+tokenizeScenarios : Test
+tokenizeScenarios =
+    describe "tokenize: from (k,v) to ([tokens], v)"
+        [ test "tokenize 1: empty" <|
+            \_ -> testTokenize ( "", "" ) ( [], "" )
+        , test "tokenize 2: single value" <|
+            \_ -> testTokenize ( "", "a" ) ( [], "a" )
+        , test "tokenize 3: single token" <|
+            \_ -> testTokenize ( "a", "" ) ( [ "a" ], "" )
+        , test "tokenize 4: two tokens" <|
+            \_ -> testTokenize ( "a_b", "" ) ( [ "a", "b" ], "" )
+        , test "tokenize 5: ignore empty tokens 1" <|
+            \_ -> testTokenize ( "_", "" ) ( [], "" )
+        , test "tokenize 5: ignore empty tokens 2" <|
+            \_ -> testTokenize ( "_a___", "" ) ( [ "a" ], "" )
+        ]
+
+
+removeNumericTokensScenarios : Test
+removeNumericTokensScenarios =
+    describe "removeNumericTokens"
+        [ test "removeNumericTokens 1: empty" <|
+            \_ -> testRemoveNumericTokens ( [], "" ) ( [], "" )
+        , test "removeNumericTokens 2: a numberic token" <|
+            \_ -> testRemoveNumericTokens ( [ "1" ], "" ) ( [], "" )
+        , test "removeNumericTokens 3: value" <|
+            \_ -> testRemoveNumericTokens ( [ "a" ], "a" ) ( [ "a" ], "a" )
+        , test "removeNumericTokens 4: mixed values" <|
+            \_ ->
+                testRemoveNumericTokens
+                    ( [ "a", "1", "2", "b" ], "a" )
+                    ( [ "a", "b" ], "a" )
+        ]
+
+
+getPathScenarios : Test
+getPathScenarios =
+    describe "getPath: (['a', 'b', 'c'], 'v') -> (['a', 'b'], (Just 'c', 'v'))"
+        [ test "getPath: ([], '') -> ([], (Nothing, ''))" <|
+            \_ -> testGetPath ( [], "" ) ( [], ( Nothing, "" ) )
+        , test "getPath: ([], 'a') -> ([], (Nothing, 'a'))" <|
+            \_ -> testGetPath ( [], "a" ) ( [], ( Nothing, "a" ) )
+        , test "getPath: (['x'], 'a') -> ([], (Just 'x', 'a'))" <|
+            \_ -> testGetPath ( [ "x" ], "a" ) ( [], ( Just "x", "a" ) )
+        , test "getPath: (['x', 'y'], 'a') -> (['x'], (Just 'y', 'a'))" <|
+            \_ -> testGetPath ( [ "x", "y" ], "a" ) ( [ "x" ], ( Just "y", "a" ) )
+        ]
+
+
+toNodeScenarios : Test
+toNodeScenarios =
+    describe "toNode: (['a','b'], (Just 'c','v')) -> (['a','b'], Element '<c>v</c>')"
+        [ test "toNode: ([], (Nothing, '')) -> ([], None)" <|
+            \_ -> testToNode ( [], ( Nothing, "" ) ) ( [], C.None )
+        , test "toNode: ([], (Just 'x', 'a')) -> ([], Element '<x>a</x>')" <|
+            \_ ->
+                testToNode ( [], ( Just "x", "a" ) )
+                    ( [], C.Element (X.element "x" [] [ X.text "a" ]) )
+        , test "toNode: (['p'], (Just 'x', 'a')) -> (['p'], Element '<x>a</x>')" <|
+            \_ ->
+                testToNode ( [ "p" ], ( Just "x", "a" ) )
+                    ( [ "p" ], C.Element (X.element "x" [] [ X.text "a" ]) )
+        , test "toNode: (['p'], (Just '@x', 'a')) -> (['p'], Attribute 'x'='a')" <|
+            \_ ->
+                testToNode ( [ "p" ], ( Just "@x", "a" ) )
+                    ( [ "p" ], C.Attribute ( "x", "a" ) )
+        ]
+
+
+convertSingleItemScenarios : Test
+convertSingleItemScenarios =
+    describe "convertSingleItem: (key,value) -> (Path, Element '<x>value</x>')"
+        [ test "convertSingleItem: None" <|
+            \_ -> testConvertSingleItem ( "1_2_3", "value" ) ( [], C.None )
+        , test "convertSingleItem: element" <|
+            \_ ->
+                testConvertSingleItem ( "A_B", "v" )
+                    ( [ "A" ], makeNodeElement "B" "v" )
+        , test "convertSingleItem: attribute" <|
+            \_ ->
+                testConvertSingleItem ( "A_B_@c", "v" )
+                    ( [ "A", "B" ], C.Attribute ( "c", "v" ) )
+        ]
+
+
+convertScenarios : Test
+convertScenarios =
     describe "preparation of input tests"
-        [ describe "tokenize: from (k,v) to ([tokens], v)"
-            [ test "tokenize 1: empty" <|
-                \_ -> testFromKvToTokensV ( "", "" ) ( [], "" )
-            , test "tokenize 2: single value" <|
-                \_ -> testFromKvToTokensV ( "", "a" ) ( [], "a" )
-            , test "tokenize 3: single token" <|
-                \_ -> testFromKvToTokensV ( "a", "" ) ( [ "a" ], "" )
-            , test "tokenize 4: two tokens" <|
-                \_ -> testFromKvToTokensV ( "a_b", "" ) ( [ "a", "b" ], "" )
-            , test "tokenize 5: ignore empty tokens 1" <|
-                \_ -> testFromKvToTokensV ( "_", "" ) ( [], "" )
-            , test "tokenize 5: ignore empty tokens 2" <|
-                \_ -> testFromKvToTokensV ( "_a___", "" ) ( [ "a" ], "" )
-            ]
-        , describe "removeNumericTokens"
-            [ test "removeNumericTokens 1: empty" <|
-                \_ -> testRemoveNumericTokens ( [], "" ) ( [], "" )
-            , test "removeNumericTokens 2: a numberic token" <|
-                \_ -> testRemoveNumericTokens ( [ "1" ], "" ) ( [], "" )
-            , test "removeNumericTokens 3: value" <|
-                \_ -> testRemoveNumericTokens ( [ "a" ], "a" ) ( [ "a" ], "a" )
-            , test "removeNumericTokens 4: mixed values" <|
-                \_ ->
-                    testRemoveNumericTokens
-                        ( [ "a", "1", "2", "b" ], "a" )
-                        ( [ "a", "b" ], "a" )
-            ]
-        , describe "getPath: (['a', 'b', 'c'], 'v') -> (['a', 'b'], (Just 'c', 'v'))"
-            [ test "getPath: ([], '') -> ([], (Nothing, ''))" <|
-                \_ -> testGetPath ( [], "" ) ( [], ( Nothing, "" ) )
-            , test "getPath: ([], 'a') -> ([], (Nothing, 'a'))" <|
-                \_ -> testGetPath ( [], "a" ) ( [], ( Nothing, "a" ) )
-            , test "getPath: (['x'], 'a') -> ([], (Just 'x', 'a'))" <|
-                \_ -> testGetPath ( [ "x" ], "a" ) ( [], ( Just "x", "a" ) )
-            , test "getPath: (['x', 'y'], 'a') -> (['x'], (Just 'y', 'a'))" <|
-                \_ -> testGetPath ( [ "x", "y" ], "a" ) ( [ "x" ], ( Just "y", "a" ) )
-            ]
-        , describe "toNode: (['a', 'b'], (Just 'c', 'v')) -> (['a', 'b'], Element '<c>v</c>')"
-            [ test "toNode: ([], (Nothing, '')) -> ([], None)" <|
-                \_ -> testToNode ( [], ( Nothing, "" ) ) ( [], C.None )
-            , test "toNode: ([], (Just 'x', 'a')) -> ([], Element '<x>a</x>')" <|
-                \_ ->
-                    testToNode ( [], ( Just "x", "a" ) )
-                        ( [], C.Element (X.element "x" [] [ X.text "a" ]) )
-            , test "toNode: (['p'], (Just 'x', 'a')) -> (['p'], Element '<x>a</x>')" <|
-                \_ ->
-                    testToNode ( [ "p" ], ( Just "x", "a" ) )
-                        ( [ "p" ], C.Element (X.element "x" [] [ X.text "a" ]) )
-            , test "toNode: (['p'], (Just '@x', 'a')) -> (['p'], Attribute 'x'='a')" <|
-                \_ ->
-                    testToNode ( [ "p" ], ( Just "@x", "a" ) )
-                        ( [ "p" ], C.Attribute ( "x", "a" ) )
-            ]
-        , describe "convertSingleItem: (key,value) -> (Path, Element '<x>value</x>')"
-            [ test "convertSingleItem: None" <|
-                \_ -> testConvertSingleItem ( "1_2_3", "value" ) ( [], C.None )
-            , test "convertSingleItem: element" <|
-                \_ ->
-                    testConvertSingleItem ( "A_B", "v" )
-                        ( [ "A" ], makeNodeElement "B" "v" )
-            , test "convertSingleItem: attribute" <|
-                \_ ->
-                    testConvertSingleItem ( "A_B_@c", "v" )
-                        ( [ "A", "B" ], C.Attribute ( "c", "v" ) )
-            ]
-        , describe "convert: transform key-values to reduced Node"
+        [ describe "convert: transform key-values to reduced Node"
             [ test "convert: attempt 1: func returns the root item" <|
                 \_ -> testConvert alwaysPrevious (makeEmptyPathNode "ParentOfRoot")
             , test "convert: attempt 2: func returns the last item" <|
@@ -85,8 +105,8 @@ testPreparation =
         ]
 
 
-testFromKvToTokensV : C.KeyValue -> C.TokensValue -> Expect.Expectation
-testFromKvToTokensV input expected =
+testTokenize : C.KeyValue -> C.TokensValue -> Expect.Expectation
+testTokenize input expected =
     input
         |> C.tokenize
         |> Expect.equal expected
