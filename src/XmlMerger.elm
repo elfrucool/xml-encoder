4429a235ff2ed65bd23813c8abbfa32907474f19
gserrano <gserrano@nearsoft.com>
2018-02-15 18:56:09 -0600

    Exposing XmlMerger as module

--- src/XmlMerger.elm
+++ src/XmlMerger.elm
@@ -1,10 +1,23 @@
-module XmlMerger exposing (..)
+module XmlMerger exposing (merge)
+
+{-| This library is for merging XmlGenericConverter.PathNode elements
+to produce a reduced XmlGenericConverter.PathNode with appended values.
+
+# Merge functions
+@docs merge
+-}
 
 import XmlNode as X
 import XmlGenericConverter as C
 import Maybe.Extra as ME
 
 
+{-| Merges a XmlGenericConverter.PathNode into another
+XmlGenericConverter.PathNode either as a child or as an attribute depending
+on its type.
+
+    merge child parent
+-}
 merge : C.PathNode -> C.PathNode -> C.PathNode
 merge ( curPath, curNode ) ( prevPath, prevNode ) =
     let
